1dde39a337372a03e21b3ca7a29e38a53e8625e8
iatamy <iatamybilton@outlook.com>
2024-05-31 19:58:27 -0300

    criação do mvc e implementação de testes de validação do cpf e uso a api externa para verificação do cep

---
+++ Controller/controller.js
@@ -0,0 +1,4 @@
+// aqui será o colocado codigos responsaveis por ações como: limpar formulario, pegar os dados e mandar eles interagirem com a model
+// fazer como que os dados aqui colocados, sejam mandados para a model e assim mandar para o banco de dados.
+
+
