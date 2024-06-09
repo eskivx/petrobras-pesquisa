5cf556bc1983ed1dfbb01811252e601157afec55
iatamy <iatamybilton@outlook.com>
2024-06-09 14:28:20 -0300

    implementando o consultacep na pagina de cadastro

--- backend/consultacep.js
+++ backend/consultacep.js
@@ -1,20 +1,21 @@
 function limpa_formulário_cep() {
     //Limpa valores do formulário de cep.
-    document.getElementById('rua').value=("");
-    document.getElementById('bairro').value=("");
-    document.getElementById('cidade').value=("");
-    document.getElementById('uf').value=("");
-    document.getElementById('ibge').value=("");
+    document.getElementById('cep').value=("");
+    
 }
 
 function meu_callback(conteudo) {
 if (!("erro" in conteudo)) {
     //Atualiza os campos com os valores.
-    document.getElementById('rua').value=(conteudo.logradouro);
-    document.getElementById('bairro').value=(conteudo.bairro);
-    document.getElementById('cidade').value=(conteudo.localidade);
-    document.getElementById('uf').value=(conteudo.uf);
-    document.getElementById('ibge').value=(conteudo.ibge);
+    /* document.getElementById('rescep').innerHTML=(conteudo.logradouro); */
+    var localidade = String(conteudo.localidade)
+    if (localidade == "Florianópolis" || 'Tijucas') {
+        
+        document.getElementById('rescep').innerHTML="cep valido"
+        
+    }
+    
+    
 } //end if.
 else {
     //CEP não Encontrado.
@@ -37,13 +38,6 @@ if (cep != "") {
     //Valida o formato do CEP.
     if(validacep.test(cep)) {
 
-        //Preenche os campos com "..." enquanto consulta webservice.
-        document.getElementById('rua').value="...";
-        document.getElementById('bairro').value="...";
-        document.getElementById('cidade').value="...";
-        document.getElementById('uf').value="...";
-        document.getElementById('ibge').value="...";
-
         //Cria um elemento javascript.
         var script = document.createElement('script');
 
@@ -63,6 +57,8 @@ if (cep != "") {
 else {
     //cep sem valor, limpa formulário.
     limpa_formulário_cep();
+    document.getElementById('rescep').innerHTML=""
+    
 }
 };
 
