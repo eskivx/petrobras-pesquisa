0f07aaaf0da159579e410fc0272fd0dd7a3e54bc
André Branco <andre_c_branco@estudante.sc.senai.br>
2024-06-22 19:16:44 -0300

    paginas de perguntas criadas

--- front-end/src/pages/Pergunta4/index.jsx
+++ front-end/src/pages/Pergunta4/index.jsx
@@ -1,46 +1,41 @@
+import '../../index.css'
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../../../images/logo-petrobras.svg';
 import logo2 from '../../../../images/nome-petrobras.svg';
-import { Navbar } from '../../componentes/Navbar';
-import {Pergunta2} from '../Pergunta2'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
-export function Pesquisa() {
+export function Pergunta4() {
     return (
-        <>
-        <div className="tudo div-master">
-            <Navbar/>
             <div className="div-container gradient-background div-master">
                 <div className="main-div justified-center my-5 round-corner">
                     <div className="container" id="container-home">
                         <h1 className="my-4" id="h1-home">Pesquisa</h1>
-                        <p>Qual é a sua renda mensal?</p>
+                        <p>Você conhece o projeto da Petrobras próxima da sua casa?</p>
                         <div>
                             <input type="radio" id="vermelho" name="cor" value="vermelho" />
-                            <label htmlFor="vermelho">Acima de sete salários mínimos</label><br />
+                            <label htmlFor="vermelho">Não</label><br />
                         </div>
                         <div>
                             <input type="radio" id="azul" name="cor" value="azul" />
-                            <label htmlFor="azul">Acima de cinco salários mínimos</label><br />
+                            <label htmlFor="azul">Muito pouco</label><br />
                         </div>
                         <div>
                             <input type="radio" id="verde" name="cor" value="verde" />
-                            <label htmlFor="verde">Acima de três salários mínimos</label><br />
+                            <label htmlFor="verde">Pouco</label><br />
                         </div>
                         <div>
                             <input type="radio" id="amarelo" name="cor" value="amarelo" />
-                            <label htmlFor="amarelo">Acima de três salários mínimos</label><br />
+                            <label htmlFor="amarelo">Sim</label><br />
                         </div>
                         <div>
                             <input type="radio" id="amarelo" name="cor" value="amarelo" />
-                            <label htmlFor="amarelo">Abaixo de três salários mínimos</label><br />
+                            <label htmlFor="amarelo">Muito</label><br />
                         </div>
-                        <Link to="/pergunta2"><button className="btn btn-dark btn-lg my-5 col-6" type="button">Responder</button></Link>
+                        <Link to="/pergunta5"><button className="btn btn-dark btn-lg my-5 col-6" type="button">Responder</button></Link>
                     </div>
                 </div>
             </div>
-        </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossOrigin="anonymous"></script>
-        </>
-    );
-}
+            );
+        };
--- front-end/src/routes/auth.routes.jsx
+++ front-end/src/routes/auth.routes.jsx
@@ -7,6 +7,15 @@ import { Login } from '../pages/Login'
 import { Perfil_acessar} from '../pages/Perfil-acessar'
 import { Perfil_alterar } from '../pages/Perfil-alterar'
 import { Pesquisa } from '../pages/Pesquisa'
+import { Pergunta2 } from '../pages/Pergunta2'
+import { Pergunta3 } from '../pages/Pergunta3'
+import { Pergunta4 } from '../pages/Pergunta4'
+import { Pergunta5 } from '../pages/Pergunta5'
+import { Pergunta6 } from '../pages/Pergunta6'
+import { Pergunta7 } from '../pages/Pergunta7'
+import { Pergunta8 } from '../pages/Pergunta8'
+import { Pergunta9 } from '../pages/Pergunta9'
+import { Pergunta10 } from '../pages/Pergunta10'
 
 
 
@@ -22,6 +31,15 @@ export function AuthRoutes() {
      <Route path='/perfil-acessar' element={<Perfil_acessar/>}/>
      <Route path='/perfil-alterar' element={<Perfil_alterar/>}/>
      <Route path='/pesquisa' element={<Pesquisa/>}/>
+     <Route path='/pergunta2' element={<Pergunta2/>}/>
+     <Route path='/pergunta3' element={<Pergunta3/>}/>
+     <Route path='/pergunta4' element={<Pergunta4/>}/>
+     <Route path='/pergunta5' element={<Pergunta5/>}/>
+     <Route path='/pergunta6' element={<Pergunta6/>}/>
+     <Route path='/pergunta7' element={<Pergunta7/>}/>
+     <Route path='/pergunta8' element={<Pergunta8/>}/>
+     <Route path='/pergunta9' element={<Pergunta9/>}/>
+     <Route path='/pergunta10' element={<Pergunta10/>}/>
    </Routes>
  )
 }
